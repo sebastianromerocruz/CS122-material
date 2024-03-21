7f42281a676c4870c4a7523438f0c89b221adf09
Sebastián Romero Cruz <sebastianromerocruz@gmail.com>
2024-03-20 22:23:51 -0400

    Pushing week 9, day 2 lectures

---
+++ src/15-interfaces/code/MusicalInstrument.java
@@ -0,0 +1,10 @@
+package interfaces.interfaces;
+
+public interface MusicalInstrument {
+
+    /**
+     * We know that we want our musical instruments to play—it's just not
+     * important for us to define HOW just yet.
+     * */
+    public void play();
+}
